989769e8b306b96b092733f910920bd282e6b689
AnotherRizz <risqijapana55368@gmail.com>
2024-06-27 10:33:55 +0700

    todolist

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Todo\TodoController; // Pastikan namespace benar
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
